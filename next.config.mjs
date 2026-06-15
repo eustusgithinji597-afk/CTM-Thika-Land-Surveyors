1b00e3b35b593f22b35671176970e990eb71121b
kimani <fackerjames40@gmail.com>
2026-06-15 16:31:33 +0300

    chore: whitelist supabase storage domain for image rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nkkcyduvuasiyqcznbhb.supabase.co', // Your exact Supabase project reference id domain
+        port: '',
+        pathname: '/storage/v1/object/public/**', // Allows access to all your public storage buckets
+      },
+    ],
   },
 };
 
